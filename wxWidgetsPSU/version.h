e7ccf069d3184a23668f7371a06322c4d4b67ad0
ben <Ben_Hsieh@APITECT.com.tw>
2016-09-10 17:28:20 +0800

    Version 1.0.0.9
    Signed-off-by: ben <Ben_Hsieh@APITECT.com.tw>

--- wxWidgetsPSU/version.h
+++ wxWidgetsPSU/version.h
@@ -23,7 +23,7 @@
 /* ---------- ---------- ---------- ---------- ---------- ---------- */
 /* |     1.0.0.8        |     Support Set USB Adaptor I2C BitRate   |*/
 /* ---------- ---------- ---------- ---------- ---------- ---------- */
-/* |     1.0.0.9        |     Support Set USB Adaptor GPIO Finction |*/
+/* |     1.0.0.9        |     Support Set USB Adaptor GPIO Function |*/
 /* ---------- ---------- ---------- ---------- ---------- ---------- */
 
 #define VERSION_STRING "1.0.0.9" /**< Program Version, Shown in About Dialog */
