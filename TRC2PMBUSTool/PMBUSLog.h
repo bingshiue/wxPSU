74fd44d2d1f184c78e45f5ef5821ba1b3e112876
ben <Ben_Hsieh@APITECT.com.tw>
2017-05-26 16:01:44 +0800

    Avoid VC 15 2017 C++ IntelliSense Parse Error
    Signed-off-by: ben <Ben_Hsieh@APITECT.com.tw>

--- TRC2PMBUSTool/PMBUSLog.h
+++ TRC2PMBUSTool/PMBUSLog.h
@@ -36,15 +36,15 @@ enum DEBUG_MSG_LEVEL {
 #define PSU_DEBUG_PRINT(level,fmt, ...)                                             \
 		if(level <= DEFAULT_DEBUG_MSG_LEVEL){                                       \
 				if(level == MSG_FATAL)                                              \
-			wxLogFatalError("%-20s %s():%d:"fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);   \
+			wxLogFatalError("%-20s %s():%d:"##fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);   \
                 else if(level == MSG_ERROR)                                         \
-		    wxLogError("%-20s %s():%d:"fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);        \
+		    wxLogError("%-20s %s():%d:"##fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);        \
 				else if(level == MSG_ALERT)                                         \
-		    wxLogMessage("%-20s %s():%d:"fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);      \
+		    wxLogMessage("%-20s %s():%d:"##fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);      \
 				else if(level == MSG_DEBUG)                                         \
-		    wxLogDebug("%-20s %s():%d:"fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);        \
+		    wxLogDebug("%-20s %s():%d:"##fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);        \
 				else if(level == MSG_DETAIL)                                        \
-		    wxLogVerbose("%-20s %s():%d:"fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);      \
+		    wxLogVerbose("%-20s %s():%d:"##fmt,PMBUSHelper::GetNowTimeString().c_str(),__FUNCTION__,__LINE__,## __VA_ARGS__);      \
 		}
 
 #else
@@ -53,15 +53,15 @@ enum DEBUG_MSG_LEVEL {
 #define PSU_DEBUG_PRINT(level,fmt, ...)         \
 		if(level <= DEFAULT_DEBUG_MSG_LEVEL){   \
 				if(level == MSG_FATAL)          \
-			wxLogFatalError("%-20s"fmt,PMBUSHelper::GetNowTimeString().c_str(), ## __VA_ARGS__);   \
+			wxLogFatalError("%-20s"##fmt,PMBUSHelper::GetNowTimeString().c_str(), ## __VA_ARGS__);   \
 				else if(level == MSG_ERROR)     \
-		    wxLogError("%-20s"fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);        \
+		    wxLogError("%-20s"##fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);        \
 				else if(level == MSG_ALERT)     \
-		    wxLogMessage("%-20s"fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);      \
+		    wxLogMessage("%-20s"##fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);      \
 				else if(level == MSG_DEBUG)     \
-		    wxLogDebug("%-20s"fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);        \
+		    wxLogDebug("%-20s"##fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);        \
 				else if(level == MSG_DETAIL)    \
-		    wxLogVerbose("%-20s"fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);      \
+		    wxLogVerbose("%-20s"##fmt,PMBUSHelper::GetNowTimeString().c_str(),## __VA_ARGS__);      \
 		}
 
 #endif // #ifdef PSU_DEBUG_MSG
